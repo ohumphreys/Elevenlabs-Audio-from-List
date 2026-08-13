4b6d1017766e959080cb6e6977eab1aab4b2fc72
ohumphreys <onh@stanford.edu>
2026-08-12 23:15:56 -0700

    a brief reminder of how bad this would have been if I needed to make the pronuniciationd ictionary at runtime before I fix that

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
-from elevenlabs.client import ElevenLabs
-import time
 import os
+from elevenlabs.client import ElevenLabs
+from elevenlabs import PronunciationDictionaryVersionLocator
 
 load_dotenv()
 
@@ -11,7 +11,7 @@ client = ElevenLabs(
 
 
 # The voices that I'm using by demographic property
-# names are of formate [B|W] [S|N] [F|M], 
+# names are of format [B|W] [S|N] [F|M], 
 # where B/W refers to Black or White, S|N refers to Southern or Non-Southern, and F|M refers to Female or Male
 voice_keys = {
     'BNF' : 'NQMJRVvPew6HsaebYnZj', # Black, Non-Southern, Female - 'Cecily'
@@ -21,7 +21,7 @@ voice_keys = {
     'BNM' : 'pQh9V7vKVWKF3pBFDSc5', # Black, Non-Southern, Male - 'Miles'
     'BSF' : 'fLQhkOW7F9KVKAjYCbhr', # Black, Southern, Female - 'CiCi'
     'WNM' : 'ljX1ZrXuDIIRVcmiVSyR', # White, Non-Southern, Male - 'Michael'
-    'WNF' : 'l4Coq6695JDX9xtLqXDE' # White, Non-Southern, Female - 'Lauren'
+    'WNF' : 'l4Coq6695JDX9xtLqXDE', # White, Non-Southern, Female - 'Lauren'
 }
 
 
@@ -31,22 +31,41 @@ voice_keys = {
 # #     print(client.voices.get(voice_keys[name]).name)
 
 
-VOICE_BEING_USED = voice_keys['WNM'] # using a research-typical voice to start
+# VOICE_BEING_USED = voice_keys['WNM']
+VOICE_BEING_USED = 'JBFqnCBsd6RMkjVDRZzb', # temporarily testing with a free voice
 
-words = ['Testing', "Tebble"]
+# with open("pronunciation_guide.pls", "rb") as f:
+#     pronunciation_dictionary = client.pronunciation_dictionaries.create_from_file(
+#         file=f.read(), name="Pseudoword Dictionary"
+#     )
 
-for word in words:
-    response = client.text_to_speech.convert(
-        text=word,
-        voice_id='JBFqnCBsd6RMkjVDRZzb', # temporarily testing with a free voice
-        model_id="eleven_multilingual_v2",
-        output_format='mp3_44100_64',
-        previous_text="Here is a word: ",
-        next_text="."
-    )
+pronunciation_dictionary = client.pronunciation_dictionaries.get('y94uKypoHq7oFFtu8A1U')
 
-    with open(f'{word}.mp3', 'wb') as f:
-        for chunk in response:
-            f.write(chunk)
+print(pronunciation_dictionary.name)
+print(pronunciation_dictionary.rules)
+print(pronunciation_dictionary.id)
+print(pronunciation_dictionary.latest_version_id)
+
+# words = ['Normal', 'Changed']
+
+# for word in words:
+#     response = client.text_to_speech.convert(
+#         text=word,
+#         voice_id=VOICE_BEING_USED,
+#         model_id="eleven_flash_v2",
+#         output_format='mp3_44100_64',
+#         # previous_text="Here is a word: ",
+#         # next_text=".",
+#         pronunciation_dictionary_locators=[
+#             PronunciationDictionaryVersionLocator(
+#                 pronunciation_dictionary_id=pronunciation_dictionary.id,
+#                 version_id=pronunciation_dictionary.version_id,
+#             )
+#         ],
+#     )
+
+#     with open(f'output/{word}.mp3', 'wb') as f:
+#         for chunk in response:
+#             f.write(chunk)
 
 
