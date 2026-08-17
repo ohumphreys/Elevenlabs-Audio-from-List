89ed653e0cf2a8c47b3be1abfa1cb68c012fa53e
ohumphreys <onh@stanford.edu>
2026-08-17 16:53:42 -0700

    Finished up with WNM

--- script.py
+++ script.py
@@ -53,8 +53,8 @@ def record_word(word: str):
         model_id="eleven_flash_v2",
         language_code='en',
         voice_settings=VoiceSettings(speed=0.8),
-        previous_text= "Here is a word, which I will pronounce slowly and clearly: ",
-        next_text=".",
+        # previous_text= "Here is a word, which I will pronounce slowly and clearly: ",
+        # next_text=".",
         output_format='mp3_44100_128',
         pronunciation_dictionary_locators=[
             PronunciationDictionaryVersionLocator(
@@ -69,12 +69,12 @@ def record_word(word: str):
             f.write(chunk)
             
 
-FOLDER_PREFIX = "WNM/pseudo/one syllable"
-with open('input/pseudo_one_syllable_TEST.csv', newline='') as f:
-    words = [row[0] for row in csv.reader(f)]
+# FOLDER_PREFIX = "WNM/pseudo/one syllable"
+# with open('input/pseudo_one_syllable_TEST.csv', newline='') as f:
+#     words = [row[0] for row in csv.reader(f)]
 
-# FOLDER_PREFIX = "rerecords"
-# words = ['Tebble', 'Paff', 'Kug']
+FOLDER_PREFIX = "rerecords"
+words = ['Top']
 
 for word in words:
     record_word(word)
